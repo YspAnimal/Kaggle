22575fe224365f297021398b61ee067ea19b47df
YspAnimal <Animal1986@mail.ru>
2017-01-31 16:33:13 +0400

    1

--- Rest/RestRev.R
+++ Rest/RestRev.R
@@ -13,7 +13,7 @@ if (file.exists(destFile)){
     ResReviewDataTrain <- fread(destFile, nrows = 36024)#, sep= "?", header = FALSE)
     #ResReviewDataTrain2 <- fread(destFile, sep= "?", header = FALSE)
 }
-ResReviewDataTrain <- mutate(ResReviewDataTrain, review = ifelse(V1>=4, 1, 0)) %>% select(- V1)
+ResReviewDataTrain <- mutate(ResReviewDataTrain, review = ifelse(V1>=4, 2, 0)) %>% select(- V1)
 DataTrainPos <- filter(ResReviewDataTrain, review == 1)
 DataTrainNeg <- filter(ResReviewDataTrain, review == 0)
 
@@ -28,7 +28,7 @@ Pos.cor <- tm_map(Pos.cor, toSpace, "@[^\\s]+") %>%
             tm_map(removeWords, stopwords("english")) %>%
             tm_map(stripWhitespace)
 DTMPos <- DocumentTermMatrix(Pos.cor)
-DTMPos <- removeSparseTerms(DTMPos, 0.98)
+#DTMPos <- removeSparseTerms(DTMPos, 0.98)
 
 Neg.cor <- VectorSource(DataTrainNeg$V2) %>% Corpus()
 meta(Pos.cor, tag = "type") <- "Negative"
@@ -40,7 +40,7 @@ Neg.cor <- tm_map(Neg.cor, toSpace, "@[^\\s]+") %>%
             tm_map(Neg.cor, removeWords, stopwords("english")) %>%
             tm_map(Neg.cor, stripWhitespace)
 DTMNeg <- DocumentTermMatrix(Neg.cor)
-DTMNeg <- removeSparseTerms(DTMNeg, 0.98)
+#DTMNeg <- removeSparseTerms(DTMNeg, 0.98)
 
 library(RWeka)
 options(mc.cores=1)
@@ -53,13 +53,65 @@ bigramNeg <- DocumentTermMatrix(Neg.cor, control = list(tokenize = BigramTokeniz
 trigramNeg <- DocumentTermMatrix(Neg.cor, control = list(tokenize = TrigramTokenizer))
 unigramNeg <- removeSparseTerms(unigramNeg, 0.98)
 bigramNeg <- removeSparseTerms(bigramNeg, 0.99)
-trigramNeg <- removeSparseTerms(trigramNeg, 0.9999)
+trigramNeg <- removeSparseTerms(trigramNeg, 0.9995)
 unigramPos <- DocumentTermMatrix(Pos.cor, control = list(tokenize = Tokenizer))
 bigramPos <- DocumentTermMatrix(Pos.cor, control = list(tokenize = BigramTokenizer))
 trigramPos <- DocumentTermMatrix(Pos.cor, control = list(tokenize = TrigramTokenizer))
 unigramPos <- removeSparseTerms(unigramPos, 0.98)
 bigramPos <- removeSparseTerms(bigramPos, 0.99)
-trigramPos <- removeSparseTerms(trigramPos, 0.9999)
+trigramPos <- removeSparseTerms(trigramPos, 0.9995)
+
+#####################################
+##   Exploratory case   #############
+#####################################
+
+GetWordFreq <- function(data) {
+    fr <- sort(colSums(as.matrix(data)), decreasing=TRUE)
+    data.frame(word=names(fr), freq=fr)
+}
+
+
+NegBiFreq <- GetWordFreq(bigramNeg)
+
+Posfreq <- sort(colSums(as.matrix(trigramPos)), decreasing=TRUE)
+Poswordfreq <- data.frame(word=names(Posfreq), freq=Posfreq)
+head(Poswordfreq,5)
+
+Negfreq <- sort(colSums(as.matrix(trigramNeg)), decreasing=TRUE)
+Negwordfreq <- data.frame(word=names(Negfreq), freq=Negfreq)
+head(Negwordfreq,5)
+
+
+makePlot <- function(data, label) {
+    ggplot(data[1:20,], aes(reorder(word, -freq), freq)) +
+        labs(x = label, y = "Frequency") +
+        theme(axis.text.x = element_text(angle = 60, size = 12, hjust = 1)) +
+        geom_bar(stat = "identity", fill = I("grey50"))
+    }
+makePlot(Poswordfreq, "20 Most Common Positive Trigram")
+makePlot(Negwordfreq, "20 Most Common Negative Trigram")
+
+makePlot(NegBiFreq, "20 Most Common Negative Bigram")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
